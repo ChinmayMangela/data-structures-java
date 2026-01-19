d01c2ab212ba8cb4cada58e8305d4c0412757a35
chinmay <mangelachinmay7@gmail.com>
2026-01-19 21:27:39 +0530

    Solved some pattern programs

--- src/patterns/Patterns.java
+++ src/patterns/Patterns.java
@@ -242,10 +242,10 @@ public class Patterns {
     public static void patternFourteen(int n) {
         /*
          *********
-          *     *
-           *   *
-            * *
-             *
+         *     *
+         *   *
+         * *
+         *
          */
         for (int i = 0; i < n; i++) {
             // for outer space
@@ -283,31 +283,134 @@ public class Patterns {
 
     public static void patternFifteen(int n) {
         /*
-              *
-             * *
-            *   *
-           *     *
-          *       *
-           *     *
-            *   *
-             * *
-              *
+         *
+         * *
+         *   *
+         *     *
+         *       *
+         *     *
+         *   *
+         * *
+         *
          */
-        for(int i = 0; i < 2 * n - 1; i++) {
-            int numberOfOuterSpace = i > n - 1 ?  i + 2 - n : n - i;
-            for(int space = 0; space < numberOfOuterSpace; space++) {
+        int helper = n - 2;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int numberOfOuterSpace = i > n - 1 ? i + 2 - n : n - i;
+            for (int space = 0; space < numberOfOuterSpace; space++) {
                 System.out.print(" ");
             }
 
-            if(i == 0 || i == 2 * n - 2) {
+            if (i == 0 || i == 2 * n - 2) {
                 System.out.print("*");
             } else {
                 System.out.print("*");
-                int numberOfInnerSpace = i > n - 1 ? 2 * (2 * n - 2 - i) - 1 : i * 2 - 1;
-                for(int space = 0; space < numberOfInnerSpace; space++) {
+
+                int numberOfInnerSpace = i > n - 1 ? (2 * (2 * n - 2 - i)) - 1
+                        : i * 2 - 1;
+                for (int innerSpace = 0; innerSpace < numberOfInnerSpace; innerSpace++) {
                     System.out.print(" ");
                 }
                 System.out.print("*");
+                helper--;
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void diamondPattern(int n) {
+        /*
+         *
+         ***
+         *****
+         *******
+         *********
+         *******
+         *****
+         ***
+         *
+         */
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int spaceCount = (i < n) ? (n - i - 1) : (i - n + 1);
+            for (int space = 0; space < spaceCount; space++) {
+                System.out.print(" ");
+            }
+
+            int starCount = (i < n) ? (2 * i + 1) : (2 * (2 * n - i - 2) + 1);
+            for (int star = 0; star < starCount; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void firstHalfOfSeventeenPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int space = 0; space < n - i - 1; space++) {
+                System.out.print(" ");
+            }
+            int numberCountForEachRow = i * 2 + 1;
+            int breakPoint = numberCountForEachRow / 2;
+            int number = i + 1;
+
+            for(int j = 0; j < numberCountForEachRow; j++) {
+                System.out.print(number);
+                if(j < breakPoint)
+                    number--;
+                else
+                    number++;
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void secondHalfOfSeventeenPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int space = 0; space < i; space++) {
+                System.out.print(" ");
+            }
+
+            int numberCountForEachRow = 2 * (n - i) - 1;
+            int breakPoint = numberCountForEachRow / 2;
+            int number = n - i;
+
+            for (int j = 0; j < numberCountForEachRow; j++) {
+                System.out.print(number);
+                if (j < breakPoint)
+                    number--;
+                else
+                    number++;
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void patternSeventeen(int n) {
+        /*
+            1
+           212
+          32123
+         4321234
+          32123
+           212
+            1
+         */
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int numberOfSpace = i < n ? n - i - 1 : i + 1 - n;
+            for (int space = 0; space < numberOfSpace; space++) {
+                System.out.print(" ");
+            }
+
+            int numberOfNumberInEachLine = (i < n) ? (2 * i + 1) : (2 * (2 * n - i - 2) + 1);
+            int breakPoint = numberOfNumberInEachLine / 2;
+            int number = i < n ? i + 1 : (2 * n) - (i + 1);
+
+            for (int j = 0; j < numberOfNumberInEachLine; j++) {
+                System.out.print(number);
+                if(j < breakPoint) number--;
+                else number++;
             }
             System.out.println();
         }
@@ -315,7 +418,7 @@ public class Patterns {
 
 
     public static void main(String[] args) {
-        patternFifteen(5);
+        patternSeventeen(5);
     }
 
 
